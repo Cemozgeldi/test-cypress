8f99e650ffa5cc4305695a23ed286f84b4452451
cozgeldi <ozgeldicem@gmail.com>
2024-03-14 21:17:50 +0300

    login function added

---
+++ cypress/e2e/ilkTestCase/loginTesti.cy.js
@@ -0,0 +1,8 @@
+describe('Login Testi',() =>{ //arrow function ile bir fonksiyon tanımladık
+
+    it('test1', () => {
+        cy.visit('https://www.https://test.urbanicfarm.com/.com/') //siteye git
+
+
+    });
+})
